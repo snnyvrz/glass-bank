884a39d527c85c47b9df1e62e59a358f4258cabe
sinaniya <sinaniya@gmail.com>
2020-06-03 02:48:47 +0430

    created CRUD endpoints

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,8 +10,35 @@ with open('./MOCK_DATA.json') as json_file:
 
 
 @app.route("/products")
-def hello():
+def get_products():
     return jsonify(products)
+
+
+@app.route("/products", methods=['POST'])
+def add_product():
+    product = request.get_json()
+    products.append(product)
+    return {'id': len(products)}, 200
+
+
+@app.route("/products/<int:id>", methods=['GET'])
+def get_product(id):
+    return jsonify(products[id - 1]), 200
+
+
+@app.route("/products/<int:id>", methods=['PUT'])
+def update_product(id):
+    product = request.get_json()
+    index = id - 1
+    products[index] = product
+    return jsonify(products[index]), 200
+
+
+@app.route("/products/<int:id>", methods=['DELETE'])
+def deleye_product(id):
+    index = id - 1
+    del products[index]
+    return 'None', 200
 
 
 app.run()
